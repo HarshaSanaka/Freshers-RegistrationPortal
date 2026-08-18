8e171834a6ce1cd9ade13141a15b88e07633d9a1
HarshaSanaka <sharshavardhan105@gmail.com>
2026-08-18 22:08:06 +0530

    feat: Add admin verification app, audit logs, and admission slip generator

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('documents/', include('documents.urls')),
     path('student/', include('students.urls')),
+    path('verification/', include('verification.urls')),
 
 ]
 
